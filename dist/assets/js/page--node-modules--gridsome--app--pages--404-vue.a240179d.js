1e2376fe93d325ba18f2d6758bc7ba54506cae58
avasconcelos114 <andre.onogoro@gmail.com>
2019-08-12 17:24:37 +0900

    Adding build to version control

---
+++ dist/assets/js/page--node-modules--gridsome--app--pages--404-vue.a240179d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{268:function(n,t,e){"use strict";e.r(t);var s=e(42),u=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("404 - not found")])},[],!1,null,null,null);t.default=u.exports}}]);
