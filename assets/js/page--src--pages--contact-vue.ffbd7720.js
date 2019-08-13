eed5aef7c4fb604d78329dafcd3285e1e19b3ce3
avasconcelos114 <andre.onogoro@gmail.com>
2019-08-13 13:27:31 +0900

    Updates

--- assets/js/page--src--pages--contact-vue.ffbd7720.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{266:function(t,n,e){"use strict";e.r(n);var o={metaInfo:{title:"Contact information"}},a=e(42),i=Object(a.a)(o,function(){var t=this.$createElement,n=this._self._c||t;return n("Layout",[n("h1",[this._v("Contact me here!")])])},[],!1,null,null,null);n.default=i.exports}}]);
