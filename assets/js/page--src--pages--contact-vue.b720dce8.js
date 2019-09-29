fe3051ab159390e799d6514445b6e67e213e982a
Andre Vasconcelos <andre.onogoro@gmail.com>
2019-09-29 13:55:13 +0900

    Updates

---
+++ assets/js/page--src--pages--contact-vue.b720dce8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{265:function(t,n,e){"use strict";e.r(n);var o={metaInfo:{title:"Contact information"}},a=e(43),i=Object(a.a)(o,(function(){var t=this.$createElement,n=this._self._c||t;return n("Layout",[n("h1",[this._v("Contact me here!")])])}),[],!1,null,null,null);n.default=i.exports}}]);
